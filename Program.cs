3471c99df9b2ad2acd49b4ce334efef6c8057d14
Евгений Лактионов <laktionov_e@t.top-academy.ru>
2026-05-07 11:13:30 +0300

    Создание массива точек

--- Program.cs
+++ Program.cs
@@ -2,9 +2,11 @@
 C# for Grandchildren: educational console application
 */
 
-using static System.Console;
+using System.Drawing;
+
 using static ConsoleLibrary.ConsoleApp;
 using static ConsoleLibrary.ConsoleIO;
+using static System.Console;
 
 WriteAppTitle("Программа...");
 Run(Action);
@@ -12,5 +14,24 @@ PromptExitApp();
 
 void Action()
 {
+    int n = ReadInteger("Введите количество точек: ");
+    PointF[] points = new PointF[n];
+    for (int i = 0; i < n; i++)
+    {
+        points[i] = ReadPointF($"Введите координаты точки {i}");
+    }
 
 }
+
+double getDistance(PointF point1, PointF point2)
+{
+    return Math.Sqrt((point2.X - point1.X) * (point2.X - point1.X) +
+        (point2.Y - point1.Y) * (point2.Y - point1.Y));
+}
+
+double getTriangleArea(double a, double b, double c)
+{
+    double p = (a + b + c) / 2;
+    return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+}
+
