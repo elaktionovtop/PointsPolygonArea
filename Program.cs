b5f58465869eb817aedc20ebd8db8b692a7bedcb
Евгений Лактионов <laktionov_e@t.top-academy.ru>
2026-05-07 11:06:49 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+C# for Grandchildren: educational console application
+*/
+
+using static System.Console;
+using static ConsoleLibrary.ConsoleApp;
+using static ConsoleLibrary.ConsoleIO;
+
+WriteAppTitle("Программа...");
+Run(Action);
+PromptExitApp();
+
+void Action()
+{
+
+}
