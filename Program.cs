a5cb0db6c46b95ebf04ef13457e92c6d8387c833
Евгений Лактионов <laktionov_e@t.top-academy.ru>
2026-05-07 11:23:44 +0300

    Полная программа

--- Program.cs
+++ Program.cs
@@ -20,6 +20,13 @@ void Action()
     {
         points[i] = ReadPointF($"Введите координаты точки {i}");
     }
+    double area = 0;
+    for(int i = 2; i < n; i++)
+    {
+        area += getTriangleArea(getDistance(points[0], points[i - 1]), getDistance(points[0], points[i]), getDistance(points[i - 1], points[i]));
+    }
+
+    WriteLine($"area = {area:F2}");
 
 }
 
